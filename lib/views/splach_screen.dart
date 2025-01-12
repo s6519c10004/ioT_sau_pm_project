13e04abe941551444739630c69526135adfe0467
s6519c10004 <s6519c10004@sau.ac.th>
2025-01-12 11:35:37 +0700

    v.3 แก้ไข splash

--- lib/views/splach_screen.dart
+++ lib/views/splach_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iot_sau_pm_project/views/signin_ui.dart';
 
 class SplachScreenUI extends StatefulWidget {
   const SplachScreenUI({super.key});
@@ -9,9 +10,50 @@ class SplachScreenUI extends StatefulWidget {
 
 class _SplachScreenUIState extends State<SplachScreenUI> {
   @override
+  void initSate() {
+    Future.delayed(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SigninUI(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: MediaQuery.of(context).size.width * 0.02,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+            ),
+            Text(
+              'IoT SAU PM Thailand',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: MediaQuery.of(context).size.height * 0.03,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
